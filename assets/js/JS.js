880f34abf6aab11424d4267a20eed7768b0c1f70
Somani Keshari <somanikeshari8726@gmail.com>
2024-11-17 02:32:42 +0530

    first commit

---
+++ assets/js/JS.js
@@ -0,0 +1,21 @@
+
+new Swiper('.swiper', {
+
+    loop: true,
+  
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+        1024:{
+            slidesPerView:2
+        },
+    }
+
+  });
